9434811ebc739236481202196251a7805105a2d2
Ahmed Bakry <bbakry20@gmail.com>
2023-08-01 06:30:02 +0300

    update 8

--- app/server.py
+++ app/server.py
@@ -2,6 +2,7 @@ import aiohttp
 import asyncio
 import uvicorn
 from fastai import *
+from fastai.vision import *
 from fastai.vision.all import *
 from io import BytesIO
 from starlette.applications import Starlette
